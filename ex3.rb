374c9c6416358a04b171faaf2fa2d2efa37a5f72
Sal Gonzalez <sega.sosickk@gmail.com>
2017-11-21 20:30:45 -0800

    first-commit

---
+++ ex3.rb
@@ -0,0 +1,24 @@
+# This will simply print out the message in the string ""
+puts "I will now count my chickens:"
+# Calculates the total number of Hens and Roosters there is.
+puts "Hens #{25 + 30 / 6}"
+puts "Roosters #{100 - 25 * 3 % 4}"
+# prints // Now i will count the eggs:
+puts "Now i will count the eggs:"
+# Simple calculation.
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+puts "Is it true that 3 + 5 < 5 - 7?"
+
+puts 3 + 5 < 5 - 7
+
+puts "What is 3 + 2? #{3 + 2}"
+puts "What is 5 - 7? #{5 - 7}"
+
+puts "Oh, that's why it's false."
+
+puts "How about somemore."
+
+puts "Is it greater? #{5 > -2}"
+puts "Is it greater or equal? #{5 >= -2}"
+puts "Is it less or equal? #{5 <= -2}"
